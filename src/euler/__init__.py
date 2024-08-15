dbbc1d28a3a9ec528ad1f966165481e6b970e7b4
Dan Strong <dan@glyx.co.uk>
2024-08-15 09:49:17 +0100

    feat: p11 and better results printing

--- src/euler/__init__.py
+++ src/euler/__init__.py
@@ -1,15 +1,38 @@
-from . import problems as p
+import time
+from typing import TypeVar, Callable
+from . import problems
+from tabulate import tabulate
+
+T = TypeVar("T")
+
+
+def timed(f: Callable[[], T]) -> tuple[int, T]:
+    start = time.time_ns()
+    res = f()
+    elapsed = time.time_ns() - start
+    ms = elapsed // 1000
+    return ms, res
+
+
+def all_problems() -> list[Callable[[], int]]:
+    ps, n = [], 1
+    try:
+        while True:
+            ps.append(getattr(problems, f"p{n}"))
+            n += 1
+    except AttributeError:
+        pass
+    return ps
 
 
 def main() -> int:
-    print(p.p1())
-    print(p.p2())
-    print(p.p3())
-    print(p.p4())
-    print(p.p5())
-    print(p.p6())
-    print(p.p7())
-    print(p.p8())
-    print(p.p9())
-    print(p.p10())
+    ps = all_problems()
+
+    results = [timed(p) for p in ps]
+    pretty = [
+        [i+1, f'{ms:,}ms', f'{result:,}'] for i, (ms, result) in enumerate(results)
+    ]
+
+    print(tabulate(pretty, stralign='right'))
+
     return 0
--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,4 +1,13 @@
-from euler.problems import fibonacci, primes, prime_factors, windows, sieve
+from euler.problems import (
+    fibonacci,
+    flip_y,
+    primes,
+    prime_factors,
+    windows,
+    sieve,
+    diagonals,
+    transpose,
+)
 from itertools import islice
 
 
@@ -93,3 +102,100 @@ def test_sieve_100():
         97,
     ]
     assert sieve(100) == up_to_100
+
+
+def test_square_diagonals():
+    arr = [
+        [0, 1],
+        [2, 3],
+    ]
+
+    assert list(diagonals(arr)) == [[1], [0, 3], [2]]
+
+
+def test_wide_diagonals():
+    arr = [
+        [0, 1, 2, 3],
+        [4, 5, 6, 7],
+    ]
+
+    assert list(diagonals(arr)) == [[3], [2, 7], [1, 6], [0, 5], [4]]
+
+
+def test_tall_diagonals():
+    arr = [
+        [0, 1],
+        [2, 3],
+        [4, 5],
+        [6, 7],
+    ]
+
+    assert list(diagonals(arr)) == [[1], [0, 3], [2, 5], [4, 7], [6]]
+
+
+def test_square_transpose():
+    arr = [
+        [0, 1],
+        [2, 3],
+    ]
+
+    assert transpose(arr) == [
+        [0, 2],
+        [1, 3],
+    ]
+
+
+def test_wide_transpose():
+    arr = [
+        [0, 1, 2, 3],
+        [4, 5, 6, 7],
+    ]
+
+    assert transpose(arr) == [
+        [0, 4],
+        [1, 5],
+        [2, 6],
+        [3, 7],
+    ]
+
+
+def test_tall_transpose():
+    arr = [
+        [0, 1],
+        [2, 3],
+        [4, 5],
+        [6, 7],
+    ]
+
+    assert transpose(arr) == [
+        [0, 2, 4, 6],
+        [1, 3, 5, 7],
+    ]
+
+
+def test_transpose_is_reversible():
+    arr = [
+        [0, 1, 2, 3],
+        [4, 5, 6, 7],
+        [8, 9, 10, 11],
+        [12, 13, 14, 15],
+        [16, 17, 18, 19],
+    ]
+
+    assert transpose(transpose(arr)) == arr
+
+
+def test_flip():
+    arr = [
+        [1, 2],
+        [3, 4],
+        [5, 6],
+        [7, 8],
+    ]
+
+    assert flip_y(arr) == [
+        [7, 8],
+        [5, 6],
+        [3, 4],
+        [1, 2],
+    ]
